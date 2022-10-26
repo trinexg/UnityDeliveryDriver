5f7faed5d9483d4328d79220aa3ea8b987afde73
Igor Karachentsev <igorkar99@gmail.com>
2022-10-26 11:45:58 -0700

    + Package pick up/drop off collision triggers

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class Delivery : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -11,6 +11,14 @@ public class Collision : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Trigger Check Works!");
+        if(other.tag == "Package")
+        {
+            Debug.Log("Picked up a package!");
+        }
+
+        if(other.tag == "Customer")
+        {
+            Debug.Log("Package Delivered!");
+        }
     }
 }
