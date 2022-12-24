2af7cecc3741e709ed54740c8e77d61f3a11b922
Igor Karachentsev <igork@Igors-Laptop.local>
2022-12-24 14:14:46 -0800

    Colors
    Added script to indicate that a package has been picked up, changes the car color to green on pick up and back to purple on delivery.

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     [SerializeField] float DestroyDelay = 1.0f;
+    [SerializeField] Color32 hasPackageColor = new Color32(1,1,1,0);
+    [SerializeField] Color32 noPackageColor = new Color32(1,0,0,0);
+
     bool hasPackage = false;
+
+    SpriteRenderer spriteRenderer;
     
+    void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>(); //Instantiate Sprite Renderer
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Collision Check Works!");
@@ -17,12 +27,14 @@ public class Delivery : MonoBehaviour
         if(other.tag == "Package" && !hasPackage)
         {
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor; //Change the color of the car on package pickup
             Debug.Log("Picked up a package!");
             Destroy(other.gameObject, DestroyDelay);
         }
 
         if(other.tag == "Customer" && hasPackage)
         {
+            spriteRenderer.color = noPackageColor; //Change the color of the car to indicate package delivery 
             Debug.Log("Package Delivered!");
             hasPackage = false;
         }
