f77895684fcd54c230dff511ede81d508c82cb48
Igor Karachentsev <igork@Igors-Laptop.local>
2022-12-24 15:11:06 -0800

    Final Commit
    Tutorial is completed and material was finalized

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -6,7 +6,9 @@ public class Driver : MonoBehaviour
 {
     //SerializeField will allow us to edit the variable in Unity
     [SerializeField] float steeringVelocity = 300;
-    [SerializeField] float movementSpeed = 17;
+    [SerializeField] float movementSpeed = 20;
+    [SerializeField] float slowSpeed = 10f;
+    [SerializeField] float boostSpeed = 30f;
     void Start()
     {
         
@@ -17,8 +19,19 @@ public class Driver : MonoBehaviour
     {
         float steerAmount = Input.GetAxis("Horizontal") * steeringVelocity * Time.deltaTime;
         float movementAmount = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
-
         transform.Rotate(0,0,-steerAmount);
         transform.Translate(0,movementAmount,0);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        movementSpeed = slowSpeed;
+    }
+    void OnTriggerEnter2D(Collider2D pawn)
+    {
+        if(pawn.tag == "Boost")
+        {
+            movementSpeed = boostSpeed;
+        }
     }
 }
