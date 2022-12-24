4a77abaa830e26ade09426fe3f508280c2044bd3
Igor Karachentsev <igork@Igors-Laptop.local>
2022-12-23 21:59:31 -0800

    Package Improvements
    Only one package can be picked up, only one can be delivered, deletes a package on pick up

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] float DestroyDelay = 1.0f;
+    bool hasPackage = false;
+    
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Collision Check Works!");
@@ -11,14 +14,17 @@ public class Delivery : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Package")
+        if(other.tag == "Package" && !hasPackage)
         {
+            hasPackage = true;
             Debug.Log("Picked up a package!");
+            Destroy(other.gameObject, DestroyDelay);
         }
 
-        if(other.tag == "Customer")
+        if(other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package Delivered!");
+            hasPackage = false;
         }
     }
 }
